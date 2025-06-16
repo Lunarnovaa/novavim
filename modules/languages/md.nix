b6ef00a741c686ce809a85f572ec9b98b83c4250
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-06-15 21:21:56 -0700

    languages/markdown: disable lsp

--- modules/languages/md.nix
+++ modules/languages/md.nix
@@ -18,7 +18,7 @@ in {
     vim.languages.markdown = {
       enable = true;
       format.enable = true; #defaults to deno
-      lsp.enable = true; #defaults to marksman
+      lsp.enable = false; #defaults to marksman
       treesitter.enable = true;
     };
   };
